626aeda6bd03879f19e05ea458e173e9476d4f67
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2012-07-20 14:50:31 +0530

    Correction with case macro

--- custom/modules/Cases/controller.php
+++ custom/modules/Cases/controller.php
@@ -11,10 +11,11 @@ class CasesController extends SugarController {
     public function action_EmailMacro() {
         $case_number = $_GET['case_number'];
         $case = new aCase();
-        $emailmacro = str_replace('%1', $case_number, $case->emailSubjectMacro);
+        $emailmacro = str_replace('%1', $case_number, $case->getEmailSubjectMacro());
         echo $emailmacro;
         exit;
     }
 
 }
+
 ?>
