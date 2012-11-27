de32deea459c25e97b2a4810ff318e0388444083
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2012-11-27 17:50:06 +0530

    Change in Contact subpanel for Target List

--- custom/Extension/modules/ProspectLists/Ext/Layoutdefs/layoutdefs.php
+++ custom/Extension/modules/ProspectLists/Ext/Layoutdefs/layoutdefs.php
@@ -13,4 +13,18 @@ $layout_defs['ProspectLists']['subpanel_setup']['leads'] = array(
         array('widget_class' => 'SubPanelTopSelectButton', 'mode' => 'MultiSelect'),
     ),
 );
+
+$layout_defs['ProspectLists']['subpanel_setup']['contacts'] = array(
+    'order' => 20,
+    'module' => 'Contacts',
+    'sort_by' => 'last_name, first_name',
+    'sort_order' => 'asc',
+    'subpanel_name' => 'forTargetList',
+    'get_subpanel_data' => 'contacts',
+    'title_key' => 'LBL_CONTACTS_SUBPANEL_TITLE',
+    'top_buttons' => array(
+        array('widget_class' => 'SubPanelTopButtonQuickCreate'),
+        array('widget_class' => 'SubPanelTopSelectButton', 'mode' => 'MultiSelect'),
+    ),
+);
 ?>
