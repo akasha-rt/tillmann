2771877d5bd9a0cc70be40831c4dec6c76fa7c14
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2012-11-27 17:37:23 +0530

    Change in Lead subpanel for Target List

---
+++ custom/Extension/modules/ProspectLists/Ext/Layoutdefs/layoutdefs.php
@@ -0,0 +1,16 @@
+<?php
+
+$layout_defs['ProspectLists']['subpanel_setup']['leads'] = array(
+    'order' => 30,
+    'module' => 'Leads',
+    'sort_by' => 'last_name, first_name',
+    'sort_order' => 'asc',
+    'subpanel_name' => 'forTargetList',
+    'get_subpanel_data' => 'leads',
+    'title_key' => 'LBL_LEADS_SUBPANEL_TITLE',
+    'top_buttons' => array(
+        array('widget_class' => 'SubPanelTopButtonQuickCreate'),
+        array('widget_class' => 'SubPanelTopSelectButton', 'mode' => 'MultiSelect'),
+    ),
+);
+?>
