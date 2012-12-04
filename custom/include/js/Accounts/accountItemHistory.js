4fc21d0f0b37ee024d713bcaec0562af5cfaf993
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2012-12-04 13:04:21 +0530

    correction to chart loading div for account

--- custom/include/js/Accounts/accountItemHistory.js
+++ custom/include/js/Accounts/accountItemHistory.js
@@ -2,6 +2,7 @@ function showItemHistoryChart(id,accName){
     var data = 'module=Accounts&action=getItemHistoryChart&id='+id + '&accName='+accName;
     $('body').append('<div id="backgroundpopup" style="display:none"></div>');
     $('body').append('<div id="historydetail_div" style="display:none" ></div>');
+    $('body').append('<div class="yui-panel-container shadow yui-overlay-hidden" id="ajaxloading_c" style="visibility: hidden; left: 668px; top: 375px; z-index: 4; display: none;"><div id="ajaxloading" class="yui-module yui-overlay yui-panel" style="visibility: inherit; width: 240px;"><div class="bd"><div id="loadingPage" align="center" style="vertical-align:middle;"><img src="themes/Sugar5/images/img_loading.gif?v=iClnhHxhIjPAAI1-ArMrWw" align="absmiddle"> <b>Loading page, please wait...</b></div></div><button style="height: 1px; width: 1px; position: absolute; left: -10000em; opacity: 0;" tabindex="-1"></button></div><div class="underlay"></div></div>');
     $('#backgroundpopup').css({
         'background': 'none repeat scroll 0 0 #000000',
         'border': '1px solid #CECECE',                                                                        
