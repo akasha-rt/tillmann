798ad8a3f8d0f7588d01188151307a2394761a78
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2012-12-04 14:25:38 +0530

    Enhancement to Chart display for account

--- custom/modules/Accounts/controller.php
+++ custom/modules/Accounts/controller.php
@@ -14,13 +14,13 @@ class AccountsController extends SugarController {
                           tasks.id,
                           tasks.date_entered AS date_entered
                         FROM tasks
-                          LEFT JOIN contacts contacts
+                          INNER JOIN contacts contacts
                             ON (tasks.contact_id = contacts.id OR tasks.parent_id = contacts.id)
                               AND contacts.deleted = 0
-                          LEFT JOIN accounts tasks_rel
+                          INNER JOIN accounts tasks_rel
                             ON tasks.parent_id = tasks_rel.id
                               AND tasks_rel.deleted = 0
-                          LEFT JOIN accounts_contacts
+                          INNER JOIN accounts_contacts
                             ON (accounts_contacts.contact_id = contacts.id
                                  OR accounts_contacts.account_id = tasks_rel.id)
                               AND accounts_contacts.deleted = 0
@@ -35,13 +35,13 @@ class AccountsController extends SugarController {
                           meetings.id,
                           meetings.date_entered AS date_entered
                         FROM meetings
-                          LEFT JOIN accounts meetings_rel
+                          INNER JOIN accounts meetings_rel
                             ON meetings.parent_id = meetings_rel.id
                               AND meetings_rel.deleted = 0
                           LEFT JOIN meetings_contacts
                             ON meetings.id = meetings_contacts.meeting_id
                               AND meetings_contacts.deleted = 0
-                          LEFT JOIN accounts_contacts
+                          INNER JOIN accounts_contacts
                             ON (accounts_contacts.contact_id = meetings_contacts.contact_id
                                  OR accounts_contacts.account_id = meetings_rel.id)
                               AND accounts_contacts.deleted = 0
@@ -56,13 +56,13 @@ class AccountsController extends SugarController {
                           calls.id,
                           calls.date_entered AS date_entered
                         FROM calls
-                         LEFT JOIN accounts calls_rel
+                         INNER JOIN accounts calls_rel
                             ON calls.parent_id = calls_rel.id
                               AND calls_rel.deleted = 0
                           LEFT JOIN calls_contacts
                             ON calls.id = calls_contacts.call_id
                               AND calls_contacts.deleted = 0
-                          LEFT JOIN accounts_contacts
+                          INNER JOIN accounts_contacts
                             ON (accounts_contacts.contact_id = calls_contacts.contact_id
                                  OR accounts_contacts.account_id = calls_rel.id)
                               AND accounts_contacts.deleted = 0
@@ -77,15 +77,15 @@ class AccountsController extends SugarController {
                           notes.id,
                           notes.date_entered AS date_entered
                         FROM notes
-                          LEFT JOIN contacts contacts
+                          INNER JOIN contacts contacts
                             ON (notes.contact_id = contacts.id
                                  OR notes.parent_id = contacts.id)
                               AND contacts.deleted = 0
-                         LEFT JOIN accounts notes_rel
+                         INNER JOIN accounts notes_rel
                             ON notes.parent_id = notes_rel.id
                               AND notes_rel.deleted = 0
                               AND notes_rel.id = '{$id}'
-                          LEFT JOIN accounts_contacts
+                          INNER JOIN accounts_contacts
                             ON (accounts_contacts.contact_id = contacts.id
                                  OR accounts_contacts.account_id = notes_rel.id)
                               AND accounts_contacts.deleted = 0
@@ -104,9 +104,10 @@ class AccountsController extends SugarController {
                               LEFT JOIN accounts
                               ON emails.parent_id = accounts.id
                               AND accounts.deleted = 0
-                              INNER JOIN emails_beans
+                              LEFT JOIN emails_beans
                                 ON emails.id = emails_beans.email_id
-                                  AND ((emails_beans.bean_module = 'Accounts' AND emails_beans.bean_id = accounts.id) OR (emails_beans.bean_module = 'Contacts' AND emails_beans.bean_id = contacts.id)) 
+                                  AND ((emails_beans.bean_module = 'Accounts' AND emails_beans.bean_id = accounts.id) 
+                                  OR (emails_beans.bean_module = 'Contacts' AND emails_beans.bean_id = contacts.id)) 
                                   AND emails_beans.deleted = 0
                              LEFT JOIN emails_text
                                 ON emails_text.email_id = emails_beans.email_id
@@ -114,46 +115,44 @@ class AccountsController extends SugarController {
                             WHERE emails.deleted = 0
                             AND (accounts.id = '{$id}'
                             OR contacts.id IN (SELECT
-                                                           contacts.id
-                                                         FROM accounts
-                                                           LEFT JOIN accounts_contacts
-                                                             ON accounts_contacts.account_id = accounts.id
-                                                             AND accounts_contacts.deleted = 0
-                                                           LEFT JOIN contacts
-                                                             ON contacts.id = accounts_contacts.contact_id
-                                                         WHERE contacts.deleted = 0
-                                                             AND accounts.deleted = 0
-                                                             AND accounts.id = '{$id}'))) 
-                            UNION ALL 
-                            (SELECT
-                              emails.id,
-                          emails.date_entered AS date_entered
-                            FROM emails
-                              JOIN (SELECT DISTINCT
-                                      email_id
-                                    FROM emails_email_addr_rel eear
-                                      JOIN email_addr_bean_rel eabr
-                                        ON ((eabr.bean_module = 'Accounts' AND eabr.bean_id = '{$id}') OR (eabr.bean_module = 'Contacts' AND eabr.bean_id IN (SELECT
-                                                           contacts.id
-                                                         FROM accounts
-                                                           LEFT JOIN accounts_contacts
-                                                             ON accounts_contacts.account_id = accounts.id
-                                                             AND accounts_contacts.deleted = 0
-                                                           LEFT JOIN contacts
-                                                             ON contacts.id = accounts_contacts.contact_id
-                                                         WHERE contacts.deleted = 0
-                                                             AND accounts.deleted = 0
-                                                             AND accounts.id = '{$id}')))
+                    contacts.id
+                    FROM accounts
+                    INNER JOIN accounts_contacts
+                    ON accounts_contacts.account_id = accounts.id
+                    AND accounts_contacts.deleted = 0
+                    INNER JOIN contacts
+                    ON contacts.id = accounts_contacts.contact_id
+                    WHERE contacts.deleted = 0
+                    AND accounts.deleted = 0
+                    AND accounts.id = '{$id}'))) 
+                                         UNION ALL 
+                                         (SELECT
+                                           emails.id,
+                                       emails.date_entered AS date_entered
+                                         FROM emails
+                                           JOIN (SELECT DISTINCT
+                                                   email_id
+                                                 FROM emails_email_addr_rel eear
+                                                   JOIN email_addr_bean_rel eabr
+                                                     ON ((eabr.bean_module = 'Accounts' AND eabr.bean_id = '{$id}') 
+                                                     OR (eabr.bean_module = 'Contacts' AND eabr.bean_id IN (SELECT
+                         contacts.id
+                         FROM accounts
+                         INNER JOIN accounts_contacts
+                         ON accounts_contacts.account_id = accounts.id
+                         AND accounts_contacts.deleted = 0
+                         INNER JOIN contacts
+                         ON contacts.id = accounts_contacts.contact_id
+                         WHERE contacts.deleted = 0
+                         AND accounts.deleted = 0
+                         AND accounts.id = '{$id}')))
 
-                                          AND eabr.email_address_id = eear.email_address_id
-                                          AND eabr.deleted = 0
-                                    WHERE eear.deleted = 0
-                            ) derivedemails
-                                ON derivedemails.email_id = emails.id
-                        LEFT JOIN emails_text
-                            ON emails_text.email_id = emails.id
-                                AND emails_text.deleted = 0
-                            WHERE emails.deleted = 0)) history WHERE YEAR(date_entered)=YEAR(CURDATE()) GROUP BY MONTH(date_entered) ORDER BY date_entered";
+                                                       AND eabr.email_address_id = eear.email_address_id
+                                                       AND eabr.deleted = 0
+                                                 WHERE eear.deleted = 0
+                                         ) derivedemails
+                                             ON derivedemails.email_id = emails.id
+                                         WHERE emails.deleted = 0)) history WHERE YEAR(date_entered)=YEAR(CURDATE()) GROUP BY MONTH(date_entered) ORDER BY date_entered";
         $result = $db->query($sql);
 
 
@@ -181,14 +180,19 @@ class AccountsController extends SugarController {
         echo'<table width="100%" border="0" cellpadding="2" cellspacing="0" class="olFgClass"><tbody>';
         echo '<tr>';
         echo '<td valign="top">';
-        echo '<fieldset><legend><strong>Account History Item Detail</strong></legend>';
+
         $status = array();
         $i = 1;
+        $sum = 0;
         while ($row = $db->fetchByAssoc($result)) {
             $status[$i]['name'] = $row['res_month'];
             $status[$i]['value'] = $row['res_count'];
+            $sum = $sum + $row['res_count'];
             $i++;
         }
+        echo '<fieldset><legend><strong>Account History Item Detail</strong></legend>';
+        echo '<span style="float:right;padding-right:20px"><strong>Total Items : ' . $sum . '</strong></span>';
+
         $strXML = "<graph xAxisName='Month' yAxisName='Item' decimalPrecision='0' formatNumberScale='0' showValues='0' numberSuffix='' yaxismaxvalue='100' >";
         $color = array('AFD8F8', 'F6BD0F', '8BBA00', 'FF8E46', '008E8E', '8E468E', '588526', '20B295', '226809', 'SCCCCC', '823A3A', 'FF0000');
         $mappingMonths = array(
