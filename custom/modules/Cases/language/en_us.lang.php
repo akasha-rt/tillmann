72809c48329750b567a4033990f32362c28b3572
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2013-01-04 15:38:52 +0530

    Two more field

--- custom/modules/Cases/language/en_us.lang.php
+++ custom/modules/Cases/language/en_us.lang.php
@@ -1,7 +1,9 @@
 <?php
-// created: 2013-01-04 14:56:08
+// created: 2013-01-04 15:33:11
 $mod_strings = array (
   'LBL_STATUS' => 'Status:',
   'LBL_CUSTOMER_EMAIL' => 'Customer Email',
   'LBL_OVERDUE_PAYMENT' => 'Over Due Payment',
+  'LBL_INVOICE_NO' => 'Invoice No',
+  'LBL_CUSTOMER_NAME' => 'Customer Name',
 );
