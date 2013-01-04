3fdc50b20bd6e196571884255c2bbb9d09b963a3
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2013-01-04 14:59:36 +0530

    Added Overdue payment fields

--- custom/modules/Cases/language/en_us.lang.php
+++ custom/modules/Cases/language/en_us.lang.php
@@ -1,5 +1,7 @@
 <?php
-// created: 2012-02-21 09:01:36
+// created: 2013-01-04 14:56:08
 $mod_strings = array (
   'LBL_STATUS' => 'Status:',
+  'LBL_CUSTOMER_EMAIL' => 'Customer Email',
+  'LBL_OVERDUE_PAYMENT' => 'Over Due Payment',
 );
