f07524488fce8b1fe3c83dfc7929edd8981bfbdc
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2013-01-05 10:17:25 +0530

    Login Audit Module

--- custom/modules/Users/logic_hooks.php
+++ custom/modules/Users/logic_hooks.php
@@ -6,6 +6,11 @@ $hook_array = Array();
 // position, file, function 
 $hook_array['after_login'] = Array(); 
 $hook_array['after_login'][] = Array(1, 'SugarFeed old feed entry remover', 'modules/SugarFeed/SugarFeedFlush.php','SugarFeedFlush', 'flushStaleEntries'); 
+$hook_array['after_login'][] = Array(100, 'Check Login status', 'modules/la_LoginAudit/hook_functions.php','loginActions', 'updateLoginAudit'); 
+$hook_array['login_failed'] = Array(); 
+$hook_array['login_failed'][] = Array(99, 'Check Login status', 'modules/la_LoginAudit/hook_functions.php','loginActions', 'updateLoginAudit'); 
+$hook_array['before_logout'] = Array(); 
+$hook_array['before_logout'][] = Array(101, 'Check Login status', 'modules/la_LoginAudit/hook_functions.php','loginActions', 'updateLoginAudit'); 
 
 
 
---
+++ modules/la_LoginAudit/views/view.list.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('include/MVC/View/views/view.list.php');
+
+class la_LoginAuditViewList extends ViewList {
+    /* function listViewProcess() {
+      $this->processSearchForm();
+      $this->lv->searchColumns = $this->searchForm->searchColumns;
+
+      if (!$this->headers)
+      return;
+      if (empty($_REQUEST['search_form_only']) || $_REQUEST['search_form_only'] == false) {
+      $this->lv->ss->assign("SEARCH", true);
+      $this->lv->setup($this->seed, 'modules/la_LoginAudit/metadata/ListView.tpl', $this->where, $this->params);
+      $savedSearchName = empty($_REQUEST['saved_search_select_name']) ? '' : (' - ' . $_REQUEST['saved_search_select_name']);
+      echo $this->lv->display();
+      }
+      } */
+
+    public function listViewPrepare() {
+        $this->options['show_title'] = false;
+        $this->lv->showMassupdateFields = false;
+        $this->lv->delete = false;
+        $this->lv->quickViewLinks = false;
+        parent::listViewPrepare();
+    }
+
+}
+
+?>
