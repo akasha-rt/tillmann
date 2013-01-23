bf7cf49bcbe1bb046e8e4268ff20ca8c2e396eec
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2013-01-23 15:38:48 +0530

    Custom lang from live

--- custom/include/language/en_us.lang.php
+++ custom/include/language/en_us.lang.php
@@ -1,1 +1,22 @@
 <?php$GLOBALS['app_list_strings']['note_type_list'] = array(    'RnD' => 'R&D',    'Watch_product' => 'Watch product',    'Problem' => 'Problem',);$GLOBALS['app_list_strings']['dom_email_status'] = array(    'archived' => 'Archived',    'closed' => 'Closed',    'draft' => 'In Draft',    'read' => 'Read',    'replied' => 'Replied',    'sent' => 'Sent',    'send_error' => 'Send Error',    'unread' => 'Unread',    'New' => 'New',    'Assigned' => 'Assigned',    'Pending' => 'Pending',);$GLOBALS['app_list_strings']['department_list'] = array(    'Operation' => 'Operation',    'Marketing' => 'Marketing',    'Accounts' => 'Accounts',    'Support' => 'Support',);$GLOBALS['app_strings']['LBL_EMAIL_DEPARTMENT'] = 'DEPARTMENT';$GLOBALS['app_strings']['LBL_EMAIL_QC_OPPORTUNITIES'] = 'Opportunity';$GLOBALS['app_list_strings']['contact_type_list'] = array(    'Enquiry' => 'Enquiry',    'Small_Molecule_Supplier' => 'Small Molecule Supplier',    'One_time_customer' => 'One_time_customer',    'Purchasing_agent' => 'Purchasing_agent',    'Regular_Customer' => 'Regular_Customer',    'Distributor' => 'Distributor',    'Support' => 'Support',    'Supplier' => 'Supplier',);$GLOBALS['app_list_strings']['case_status_dom'] = array(    'New' => 'New',    'Closed' => 'Closed',    'Pending Input' => 'Pending Input',    'Open' => 'Open',    'Followup' => 'Follow up',    'pending_customer' => 'Pending Customer',    'pending_supplier' => 'Pending Supplier',    'PO' => 'PO',);
+
+$GLOBALS['app_list_strings']['case_type_dom']=array (
+  'NONE' => 'NONE',
+  'ihcp' => 'IHCp',
+  'ihcfr' => 'IHCfr',
+  'WB' => 'WB',
+  'FACS' => 'FACS',
+  'ELISA' => 'ELISA',
+  'IF' => 'IF',
+  'IP' => 'IP',
+);
+$GLOBALS['app_list_strings']['call_status_dom']=array (
+  'Planned' => 'Planned',
+  'enquiry' => 'Enquiry',
+  'technical' => 'Technical',
+  'order' => 'Order',
+  'delivery' => 'Delivery',
+  'accounting' => 'Accounting',
+  'opportunity' => 'Opportunity',
+  'marketing' => 'Marketing',
+);
