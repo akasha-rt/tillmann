ca809f4a966d71f711f22ff67c3e604045d73646
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2013-03-06 15:21:35 +0530

    Email QuickSearch also look for Fname and Lname

--- custom/modules/Emails/controller.php
+++ custom/modules/Emails/controller.php
@@ -52,11 +52,18 @@ class EmailsController extends SugarController {
             return false;
         }
 
-        $sql = "SELECT email_addresses.email_address as email,email_addr_bean_rel.bean_id AS bean_id,email_addr_bean_rel.bean_module AS bean_module
+        $sql = "SELECT email_addresses.email_address as email,email_addr_bean_rel.bean_id AS bean_id,
+            email_addr_bean_rel.bean_module AS bean_module,accounts.name,contacts.first_name,contacts.last_name,
+            leads.first_name,leads.last_name
             FROM email_addresses
             LEFT JOIN email_addr_bean_rel ON email_addr_bean_rel.email_address_id = email_addresses.id
+            LEFT JOIN accounts ON email_addr_bean_rel.bean_id = accounts.id AND accounts.deleted = 0 
+            LEFT JOIN contacts ON email_addr_bean_rel.bean_id = contacts.id AND contacts.deleted = 0 
+            LEFT JOIN leads ON email_addr_bean_rel.bean_id = leads.id AND leads.deleted = 0 
             WHERE email_addresses.deleted = 0 AND email_addr_bean_rel.deleted = 0 
-            AND email_addresses.email_address LIKE '{$search_text}%'";
+            AND (email_addresses.email_address LIKE '{$search_text}%' OR  accounts.name LIKE '{$search_text}%' 
+                OR contacts.first_name LIKE '{$search_text}%' OR contacts.last_name LIKE '{$search_text}%'
+                OR leads.first_name LIKE '{$search_text}%' OR leads.last_name LIKE '{$search_text}%')";
 
         $rs = $db->query($sql);
         if (!$rs) {
