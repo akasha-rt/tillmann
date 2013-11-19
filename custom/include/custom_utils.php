a0155db86b36b91cc645435ee65a98ebecabea2a
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2013-11-19 17:32:10 +0530

    Add none option and cache mechanism to speed up supplier and product loading

--- custom/include/custom_utils.php
+++ custom/include/custom_utils.php
@@ -1,6 +1,9 @@
 <?php
 
 function getProductFromStoreData() {
+    if (isset(SugarCache::instance()->getProductFromStoreData)) {
+        return SugarCache::instance()->getProductFromStoreData;
+    }
     global $db;
     $dd_res = $db->query("SELECT
                             bc_storedata.name AS Product,
@@ -13,13 +16,19 @@ function getProductFromStoreData() {
                               AND bc_storedata.sku IS NOT NULL
                           GROUP BY bc_storedata.sku
                           ORDER BY bc_storedata.name");
+    $option_array = array();
+    $option_array[$dd['']] = 'None';
     while ($dd = $db->fetchByAssoc($dd_res)) {
         $option_array[$dd['Sku']] = $dd['Product'];
     }
+    SugarCache::instance()->getProductFromStoreData = $option_array;
     return $option_array;
 }
 
 function getSupplierFromStoreData() {
+    if (isset(SugarCache::instance()->getSupplierFromStoreData)) {
+        return SugarCache::instance()->getSupplierFromStoreData;
+    }
     global $db;
     $dd_res = $db->query("SELECT DISTINCT
                             bc_storedata.supplierid AS Supplier
@@ -29,9 +38,12 @@ function getSupplierFromStoreData() {
                               AND bc_storedata.supplierid IS NOT NULL
                               AND bc_storedata.supplierid != ''
                           ORDER BY bc_storedata.supplierid");
+    $option_array = array();
+    $option_array[$dd['']] = 'None';
     while ($dd = $db->fetchByAssoc($dd_res)) {
         $option_array[$dd['Supplier']] = $dd['Supplier'];
     }
+    SugarCache::instance()->getSupplierFromStoreData = $option_array;
     return $option_array;
 }
 
