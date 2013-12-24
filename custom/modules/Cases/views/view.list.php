01ed8e04dc83a4aacb191a18af7a10e4d7fee6d3
NK <no@email.com>
2013-12-24 15:14:54 +0530

    Added Product,Technical,Review and Supplier as filters in the Advanced search

--- custom/modules/Cases/views/view.list.php
+++ custom/modules/Cases/views/view.list.php
@@ -50,4 +50,56 @@ class CasesViewList extends ViewList {
         echo "<script src='custom/include/js/Home/add_follow_list.js'></script>";
     }
 
+    public function display() {
+        if (!empty($_REQUEST['product_c_advanced']) || !empty($_REQUEST['supplier_c_advanced'])) {
+            require_once 'custom/include/custom_utils.php';
+            if (!empty($_REQUEST['product_c_advanced'])) {
+                $product_temp_array = getProductName($_REQUEST['product_c_advanced']);
+                $productIDVal = array();
+                $key = 0;
+                foreach ($product_temp_array as $id => $val) {
+                    $productIDVal[$key]['id'] = $id;
+                    $productIDVal[$key]['name'] = $val;
+                    $key++;
+                }
+                $procuctJSON = json_encode($productIDVal);
+            } else {
+                $procuctJSON = '[]';
+            }
+            if (!empty($_REQUEST['supplier_c_advanced'])) {
+                $supplierList = array();
+                $supplier_temp = explode(",", $_REQUEST['supplier_c_advanced']);
+                foreach ($supplier_temp as $ids => $vals) {
+                    if (!empty($vals)) {
+                        $supplierList[$ids]['id'] = $vals;
+                        $supplierList[$ids]['name'] = $vals;
+                    }
+                }
+                $supplierJSON = json_encode($supplierList);
+            } else {
+                $supplierJSON = '[]';
+            }
+        } else {
+            $procuctJSON = '[]';
+            $supplierJSON = '[]';
+        }
+        echo "<script src='http://code.jquery.com/jquery-1.10.1.min.js'></script>
+              <script type='text/javascript' src='custom/include/js/jquery.tokeninput.js'></script>
+              <link rel='stylesheet' type='text/css' href='custom/include/css/token-input-facebook.css' />
+              <script type='text/javascript'>
+              $(document).ready(function () {
+                    $('#product_c_advanced').tokenInput('index.php?module=Cases&action=saveDataInbcDropdown&product_c=1',{prePopulate: {$procuctJSON}});
+                    $('#supplier_c_advanced').tokenInput('index.php?module=Cases&action=saveDataInbcDropdown&supplier_c=1',{prePopulate: {$supplierJSON}});
+                    $('#search_form_clear').removeAttr('onclick','').click(function(){
+                    SUGAR.searchForm.clear_form(this.form);
+                    document.getElementById(\"saved_search_select\").options[0].selected=true; 
+                    $('ul li.token-input-token').remove();
+                    return false;
+                    });    
+               });
+             </script>
+            ";
+        parent::display();
+    }
+
 }
