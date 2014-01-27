4a996154893ecf9aa041be0706c29691e5aaaecf
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2014-01-27 16:15:01 +0530

    Adding contacts via import requires last name, please make non mandatory

--- custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_last_name.php
+++ custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_last_name.php
@@ -1,6 +1,7 @@
 <?php
- // created: 2012-05-11 08:24:33
+ // created: 2014-01-27 16:14:33
 $dictionary['Contact']['fields']['last_name']['required']=false;
 $dictionary['Contact']['fields']['last_name']['unified_search']=false;
+$dictionary['Contact']['fields']['last_name']['importable']='true';
 
  ?>
