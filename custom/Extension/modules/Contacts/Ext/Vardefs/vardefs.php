68ba36dda2bd06976e1d449a662f7427fd4e9004
Dhaval Darji <dhaval@india.biztechconsultancy.com>
2014-02-01 15:56:12 +0530

    Add campaign name in Contact list

---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/vardefs.php
@@ -0,0 +1,10 @@
+<?php
+
+$dictionary['Contact']['fields']['cont_campaign_name'] =
+        array(
+            'name' => 'cont_campaign_name',
+            'type' => 'varchar',
+            'source' => 'non-db',
+            'vname' => 'LBL_CAMPAIGN_NAME_FOR_CONTACT',
+);
+?>
