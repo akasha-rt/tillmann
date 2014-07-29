32ace01e7e7f172f7c64a9dc2fb1d3a61bd7f9cc
DD <no@email.com>
2014-07-29 19:32:53 +0530

    Fix blank product name row in Product complaint dashlet-2

--- custom/modules/Cases/Dashlets/NewProductComplaintDashlet/NewProductComplaintDashlet.php
+++ custom/modules/Cases/Dashlets/NewProductComplaintDashlet/NewProductComplaintDashlet.php
@@ -78,7 +78,10 @@ class NewProductComplaintDashlet extends CustomDashletGeneric {
                                           ON cases.id = cases_cstm.id_c
                                           LEFT JOIN bc_storedata ON  FIND_IN_SET(bc_storedata.sku,cases_cstm.product_c) WHERE cases.deleted = 0
                                         AND cases_cstm.technical_c = 'Complaint'
-                                        AND (cases_cstm.product_c IS NOT NULL or cases_cstm.product_c <> '')
+                                        AND cases_cstm.product_c IS NOT NULL
+                                        AND cases_cstm.product_c <> ''
+                                        AND bc_storedata.sku <> ''
+                                        AND bc_storedata.sku IS NOT NULL
                                          GROUP BY bc_storedata.sku
                                         HAVING COUNT(bc_storedata.sku) >= 2 ) AS a",
             'custom_order_by' => $orderBy,
