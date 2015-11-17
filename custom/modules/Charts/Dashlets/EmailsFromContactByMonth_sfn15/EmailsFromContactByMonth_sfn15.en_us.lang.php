5d045de5e805cafb6e4f5a652ac656ca0c80edb4
GR <no@noemail.com>
2015-11-17 11:44:09 +0530

    Add chart for email from contact-sfn15

---
+++ custom/modules/Charts/Dashlets/EmailsFromContactByMonth_sfn15/EmailsFromContactByMonth_sfn15.en_us.lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$dashletStrings['EmailsFromContactByMonth_sfn15'] = array(
+    'LBL_TITLE' => 'Emails From Contact By Month - sfn15',
+    'LBL_DESCRIPTION' => 'Bar Chart for Emails From Contact By Month - sfn15',
+    'LBL_REFRESH' => 'Refresh Chart');
+?>
